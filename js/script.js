d02cda56d453114870eafd5f03e730187c031a97
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-05-10 02:39:52 +0000

    V5-07.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohammed All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+'use strict'
+
+function myButtonClicked() {
+  // This function does multiplication using a loop
+  // input
+  const number = parseInt(document.getElementById("number").value)
+
+  // process
+  let answer = 0
+  for (let counter = 0; counter < number; counter++) {
+    answer += counter
+  }
+  answer += number
+
+  // output
+  document.getElementById('answer').innerHTML = "The sum of all the numbers between 1 and " + number + " is " + answer
+}
